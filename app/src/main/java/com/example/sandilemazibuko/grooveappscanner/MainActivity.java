5cbe963c94b5913ff924addbe32831fd718f7a03
sandile <msentri@gmail.com>
2015-11-04 15:49:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sandilemazibuko/grooveappscanner/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.sandilemazibuko.grooveappscanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnScan = (Button)findViewById(R.id.btnScan);
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+        public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                // Handle successful scan
+                TextView txtInfo = (TextView)findViewById(R.id.txtInfo);
+                txtInfo.setText(contents);
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+                Toast.makeText(MainActivity.this, "Scanner Cancelled", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
